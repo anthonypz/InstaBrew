05ec0bd8ca8a9715c36b059021cf2e347438efbe
Anthony Perez <43166263+anthonypz@users.noreply.github.com>
2022-10-05 21:14:16 -0500

    Fix username referencing the wrong user who posted

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,6 +1,7 @@
 const cloudinary = require('../middleware/cloudinary')
 const Post = require('../models/Post')
 const Comment = require('../models/Comment')
+const User = require('../models/User')
 
 module.exports = {
   getProfile: async (req, res) => {
@@ -21,7 +22,12 @@ module.exports = {
   getFeed: async (req, res) => {
     try {
       const posts = await Post.find().sort({ createdAt: 'desc' }).lean()
-      res.render('feed.ejs', { posts: posts, user: req.user })
+      let users = []
+      for (let i in posts) {
+        let user = await User.findById(posts[i].user)
+        users.push(user.userName)
+      }
+      res.render('feed.ejs', { posts: posts, user: req.user, userName: users })
     } catch (err) {
       console.log(err)
     }
@@ -29,10 +35,16 @@ module.exports = {
   getPost: async (req, res) => {
     try {
       const post = await Post.findById(req.params.id)
+      let username = await User.findById(post.user)
       const comments = await Comment.find({ post: req.params.id })
         .sort({ createdAt: 'desc' })
         .lean()
-      res.render('post.ejs', { post: post, user: req.user, comments })
+      res.render('post.ejs', {
+        post: post,
+        user: req.user,
+        comments,
+        userName: username.userName,
+      })
     } catch (err) {
       console.log(err)
     }
