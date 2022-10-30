abf66afb0aa8702be73aae3958ae56e8f0d6f103
Anthony Perez <43166263+anthonypz@users.noreply.github.com>
2022-10-30 15:39:44 -0700

    Create route and controller logic for the gear page

--- controllers/home.js
+++ controllers/home.js
@@ -1,5 +1,11 @@
 module.exports = {
   getIndex: (req, res) => {
-    res.render("index.ejs");
+    res.render('index.ejs')
   },
-};
+  getGear: (req, res) => {
+    res.render('gear', {
+      // use es6 optional chaining for the userName
+      userName: req.user?.userName,
+    })
+  },
+}
--- routes/main.js
+++ routes/main.js
@@ -15,5 +15,6 @@ router.post('/login', authController.postLogin)
 router.get('/logout', authController.logout)
 router.get('/signup', authController.getSignup)
 router.post('/signup', authController.postSignup)
+router.get('/gear', homeController.getGear)
 
 module.exports = router
