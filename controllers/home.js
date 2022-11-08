0ba3cd1d9f4e9b37c6d0cf8a49db8d9b26651686
Anthony P <43166263+anthonypz@users.noreply.github.com>
2022-11-07 16:07:39 -0800

    Document code on home controller

--- controllers/home.js
+++ controllers/home.js
@@ -4,13 +4,13 @@ module.exports = {
   },
   getGear: (req, res) => {
     res.render('gear', {
-      // use es6 optional chaining for the userName
+      // Use es6 optional chaining for sending the userName. This data helps determine which navbar the user sees when visiting the page (logged in navbar vs logged out navbar).
       userName: req.user?.userName,
     })
   },
   getVideos: (req, res) => {
     res.render('videos', {
-      // use es6 optional chaining for the userName
+      // Use es6 optional chaining for sending the userName. This data helps determine which navbar the user sees when visiting the page (logged in navbar vs logged out navbar).
       userName: req.user?.userName,
     })
   },
